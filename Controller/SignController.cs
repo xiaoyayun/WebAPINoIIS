c230a2cfbab6ee0eafa7d3066a616cb74254d55b
xiaoyayun <xiaomufeng789@163.com>
2017-04-07 16:09:51 +0800

    WebApiNOIIS
    不要IIS发布API

---
+++ Controller/SignController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Windows.Forms;
+
+namespace WebAPi.Controller
+{
+    public class SignController:ApiController
+    {
+        [HttpGet]
+        public int Get(int id)
+        {
+            MessageBox.Show(id.ToString());
+            return id;
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(WebAPi.Startup))]
+
+namespace WebAPi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(name: "DefaultApi", routeTemplate: "api/{controller}/{id}", defaults: new { id = RouteParameter.Optional });
+            app.UseWebApi(config);
+            // 有关如何配置应用程序的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
